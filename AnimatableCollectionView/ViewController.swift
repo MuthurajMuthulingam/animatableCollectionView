dc0f99af73fa00bc96b4533d6f34fb23d889a04f
Muthuraj Muthulingam <muthurajmuthulingam@Muthurajs-MacBook-Pro-4.local>
2017-12-06 15:39:39 +0530

    Initial commit

---
+++ AnimatableCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatableCollectionView
+//
+//  Created by Muthuraj Muthulingam on 06/12/17.
+//  Copyright © 2017 Muthuraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
